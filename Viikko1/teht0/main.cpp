e56c692efaaa1d3ef2d0610c4b14b05e1941c004
Riikka Juntunen <juntunen.riikka@outlook.com>
2024-01-08 17:14:55 +0200

    Muokkaa tyylia

--- Viikko1/teht0/main.cpp
+++ Viikko1/teht0/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main()
 {
     string fname, lname;
-    cout<<"Kerro etunimesi"<<endl;
-    cin>>fname;
-    cout<<"Kerro sukunimesi"<<endl;
-    cin>>lname;
-    cout<<"Terve "<<fname<<" "<<lname<<endl;
+    cout << "Kerro etunimesi" << endl;
+    cin >> fname;
+    cout << "Kerro sukunimesi" << endl;
+    cin >> lname;
+    cout << "Terve " << fname << " " << lname << endl;
     return 0;
 }
