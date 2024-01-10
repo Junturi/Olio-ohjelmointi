245af581794e80fa8df7e102d650b4f0b77a2000
Riikka Juntunen <juntunen.riikka@outlook.com>
2024-01-10 11:36:52 +0200

    kotiteht1 osa1 valmis

--- Viikko1/kotiteht1/main.cpp
+++ Viikko1/kotiteht1/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //Generate a random number by using time as the seed
+    srand(time(NULL));
+    int randomNumber = rand() % 20;
+    randomNumber += 1;
+
+    int guessNumber = 0;
+
+    //The user will try to guess the number. User will be asked to provide a guess untill the number guessed and random number match.
+    //Program will tell the user if the guessed number is lower or higher than the random number.
+    while (guessNumber != randomNumber){
+        cout << "Arvaa luku:" << endl;
+        cin >> guessNumber;
+        if (guessNumber < randomNumber){
+            cout << "Luku on suurempi." << endl;
+        }
+        else if (guessNumber > randomNumber){
+            cout << "Luku on pienempi." << endl;
+        }
+    }
+
+    //Once the number is guessed correctly, a congratulation is printed and the program is closed.
+    cout << "Onneksi olkoon, arvasit oikein!" << endl;
+
     return 0;
 }
