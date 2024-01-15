1500a75d3c4acce6d7e3f0f80d18d5469eac9e00
Riikka Juntunen <juntunen.riikka@outlook.com>
2024-01-15 09:04:53 +0200

    Tuntiteht3, osa2 valmis

--- Viikko2/Tuntiteht3/main.cpp
+++ Viikko2/Tuntiteht3/main.cpp
@@ -4,21 +4,31 @@
  * (1) Kirjoita funktio, joka ottaa vastaan kaksi kokonaislukua.
  * Funktio kertoo molemmat luvut kahdella ja tulostaa niiden summan
  *Tulosta myös alkuperäiset luvut ennen ja jälkeen funktiokutsun.
+ *
+ *(2) Kirjoita gunktio, kuten edellä, mutta funktion on tuplattava alkuperäisten muuttujien arvot.
+ *Nyt funktiossa on otettava vastaan muuttujien osoitteet.
+ *Funktion kutsussa argumentteina annetaan muuttujien osoitteet.
 */
 
 using namespace std;
 
 void calculateProduct(int a, int b);
+void calculateProduct2(int *a, int *b);
 
 int main()
 {
     int a = 5, b = 9;
 
     cout << "Aluksi mainissa a = " << a << endl;
-    cout << "Aluksi mainissa b = " << b << endl;
+    cout << "Aluksi mainissa b = " << b << endl << endl;
+
     calculateProduct(a, b);
     cout << "Lopuksi mainissa a = " << a << endl;
-    cout << "Lopuksi mainissa b = " << b << endl;
+    cout << "Lopuksi mainissa b = " << b << endl << endl;
+
+    calculateProduct2(&a, &b);
+    cout << "Lopuksi mainissa a = " << a << endl;
+    cout << "Lopuksi mainissa b = " << b << endl << endl;
     return 0;
 }
 
@@ -27,7 +37,16 @@ void calculateProduct(int a, int b){
     b *=2;
     int sum = a+b;
 
-    cout << "Funktiossa a = " << a << endl;
-    cout << "Funktiossa b = " << b << endl;
-    cout << "Summa on " << sum << endl;
+    cout << "Funktiossa(1) a = " << a << endl;
+    cout << "Funktiossa(1) b = " << b << endl;
+    cout << "Summa(1) on " << sum << endl;
+}
+
+void calculateProduct2(int *a, int *b){
+    *a *= 2;
+    *b *= 2;
+    int sum = *a + *b;
+    cout << "Funktiossa(2) a = " << *a << endl;
+    cout << "Funktiossa(2) b = " << *b << endl;
+    cout << "Summa(2) on " << sum << endl;
 }
